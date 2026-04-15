55347a98b8a02d6840c609a6fe53df872761e880
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-04-15 17:48:05 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ashlyn
+ * Created on: Apr 2026
+ * This program uses a bluetooth radio.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distance = 0
+
+// setup
+radio.setGroup(5)
+basic.showIcon(IconNames.Happy)
+
+// button a
+basic.forever (function () {
+    distance = sonar.ping(DigitalPin.P8, DigitalPin.P12, PingUnit.Centimeters)
+    if (distance < 10) {
+        radio.sendString("Too Close")
+        basic.pause(500)
+    }
+})
+
+// reciever
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
